e84fd9c5df8d89032c084759e464efce41cc26cc
Orlando Aliaga <oaliaga@gmail.com>
2014-02-24 11:58:07 -0300

    Execute hidden in a thread

--- src/com/prey/actions/camouflage/Camouflage.java
+++ src/com/prey/actions/camouflage/Camouflage.java
@@ -37,4 +37,13 @@ public class Camouflage {
         pm.setComponentEnabledSetting(componentToEnabled, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
     	PreyLogger.i("unhide stop");
 	}
+	
+	public static void hideThread(Context context) {
+		final Context ctx=context;
+		new Thread(){
+            public void run() {
+            	Camouflage.hide(ctx, null, null); 
+            }
+		}.start();
+	}
 }
